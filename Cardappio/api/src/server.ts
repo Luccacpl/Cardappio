3b76349fb730ed259e4cae8b7b0845f593613790
Allan Cruvinel <allan.cruvinel@gmail.com>
2021-01-28 16:27:02 -0300

    adicionado algumas configurações iniciais a API

--- Cardappio/api/src/server.ts
+++ Cardappio/api/src/server.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import routes from './routes';
+import "reflect-metadata";
+import { getRepository } from 'typeorm'
+import User from './entity/User'
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
+async function save() {
+    const repository = getRepository(User);
 
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await repository.save(user);
+    console.log("entrou aqui");
+}
+save();
 
 
 console.log("App ready to use");
