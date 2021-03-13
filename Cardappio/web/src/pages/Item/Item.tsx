156a0d2ca511c3a7fe19677c7b3231291d61e8d2
Lucca Lopes <Luccacpl@gmail.com>
2021-03-12 22:33:13 -0300

    feat: Add snackbar

--- Cardappio/web/src/pages/Item/Item.tsx
+++ Cardappio/web/src/pages/Item/Item.tsx
@@ -7,6 +7,8 @@ import Cards from '../../components/Cards/Cards'
 import Container from '../../components/Container/Container'
 import { Grid } from '../../components/Grid/style'
 import Modal from '../../components/Modal/Modal'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 
 import api from '../../services/api'
 import { ChangeEventHandler } from 'react';
@@ -16,6 +18,10 @@ interface ICategory {
     id: number
 }
 
+function Alert(props: AlertProps) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 function Item() {
     const history = useHistory();
 
@@ -23,13 +29,24 @@ function Item() {
     const [categories, setCategories] = useState<ICategory[]>([]);
     const [refresh, setRefresh] = useState(0);
     const [showModal, setShowModal] = useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [openError, setOpenError] = React.useState(false);
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+        setOpenError(false);
+    };
 
     async function handleSubmit(event: ChangeEventHandler<HTMLInputElement>) {
         if (name === '') {
-            return alert('Insira um nome no campo');
+           return setOpenError(true)
         }
         else {
-            alert('Item cadastrado com sucesso')
+            setOpen(true);
 
             const data = new FormData();
 
@@ -44,6 +61,8 @@ function Item() {
             console.log({
                 name,
             })
+
+            
 
             return setShowModal(false);
         }
@@ -76,16 +95,26 @@ function Item() {
                     <Cards gridStart="4"></Cards>
                 </Grid>
             </Container>
-            {showModal === true && 
-           <Modal 
-            title="Adicionar Categoria" 
-            ButtonTitle="Adicionar" 
-            text="Digite o nome da categoria que deseja adicionar no campo abaixo." 
-            clicked={handleSubmit} 
-            value={name} 
-            change={event => setName(event.target.value)}
-            Backclicked={() => setShowModal(false)}
-            />}
+            {showModal === true &&
+                <Modal
+                    title="Adicionar Categoria"
+                    ButtonTitle="Adicionar"
+                    text="Digite o nome da categoria que deseja adicionar no campo abaixo."
+                    clicked={handleSubmit}
+                    value={name}
+                    change={event => setName(event.target.value)}
+                    Backclicked={() => setShowModal(false)}
+                />}
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Cadastro realizado com sucesso!
+                </Alert>
+            </Snackbar>
+            <Snackbar open={openError} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error">
+                    Ocorreu um erro! Cadastro não realizado
+                </Alert>
+            </Snackbar>
         </Grid>
     );
 }
