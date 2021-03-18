dbe11ec156675da7a82b7f579f7f725d741e927a
Allan Cruvinel <allan.cruvinel@gmail.com>
2021-03-17 22:32:49 -0300

    removido lib de verificação se email existe para teste

--- Cardappio/api/src/controllers/RegisterController.ts
+++ Cardappio/api/src/controllers/RegisterController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt'
 import { getRepository } from 'typeorm';
 import User from '../models/User'
-const emailExistence = require('email-existence');
+import emailExistence from 'email-existence';
 
 export default {
     async Register(req: Request, res: Response) {
@@ -16,39 +16,34 @@ export default {
         } = req.body;
 
         const encryptedPass = bcrypt.hashSync(pass, 10);
-        emailExistence.check(email, async function (err: any, checked: any) {
-            if (checked) {
-                try {
-                    const user = await repo.findOneOrFail({
-                        where: {
-                            email: email
-                        }
-                    })
-                    console.log(user);
-                    return res.status(403).json({ "ERROR": "Email already registered" })
+        try {
+            const user = await repo.findOneOrFail({
+                where: {
+                    email: email.toLowerCase()
                 }
-                catch (e) {
+            })
+            console.log(user);
+            return res.status(403).json({ "ERROR": "Email already registered" })
+        }
+        catch (e) {
 
-                    try {
-                        const user = new User();
-                        user.email = email;
-                        user.pass = encryptedPass;
-                        user.firstName = firstname;
-                        user.lastName = lastname;
-                        user.dt_nasc = dt_nasc;
-                        console.log(user);
-
-                        return res.status(201).json(await repo.save(user));
-                    }
-                    catch (e) {
-                        return res.status(500).json({ "ERROR": e.message })
-                    }
-                }
-
-            } else {
-                return res.status(404).json({ "ERROR": "mail does not exists" });
+            try {
+                const user = new User();
+                user.email = email.toLowerCase();
+                user.pass = encryptedPass;
+                user.firstName = firstname;
+                user.lastName = lastname;
+                user.dt_nasc = dt_nasc;
+                console.log(user);
+                await repo.save(user)
+                return res.status(201).json({ SUCCESS: user.email + " created" });
             }
-        });
+            catch (e) {
+                return res.status(500).json({ "ERROR": e.message })
+            }
+        }
+
+
 
 
 
